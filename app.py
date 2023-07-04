195ea9c67f0fc8b2015a86ef121018ca1a4096a4
Hadi Bagdadi <104606024+hadibagdadii@users.noreply.github.com>
2023-07-04 23:13:33 +0000

    changes to view htmls

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sign-up')
+def signup():
+    return render_template('sign-up.html')
+
+@app.route('/log-in')
+def login():
+    return render_template('log-in.html')
+
+@app.route('/student-home')
+def studenthome():
+    return render_template('student-home.html')
+
+@app.route('/teacher-home')
+def teacherhome():
+    return render_template('teacher-home.html')
+
+if __name__ == '__main__':
+    app.run()
